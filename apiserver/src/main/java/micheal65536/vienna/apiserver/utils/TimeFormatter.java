b7921f2a9ee036c9a4e4e9dcdc01ea8d07905cf1
micheal65536 <>
2024-04-09 18:08:51 +0100

    Fixed date/time format to always use UTC timezone

--- apiserver/src/main/java/micheal65536/vienna/apiserver/utils/TimeFormatter.java
+++ apiserver/src/main/java/micheal65536/vienna/apiserver/utils/TimeFormatter.java
@@ -6,11 +6,17 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.Locale;
+import java.util.TimeZone;
 
 public final class TimeFormatter
 {
 	private static final SimpleDateFormat JSON_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ROOT);
 	private static final String JSON_DURATION_FORMAT = "%d:%02d:%02d";
+
+	static
+	{
+		JSON_DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+	}
 
 	@NotNull
 	public static String formatTime(long time)
