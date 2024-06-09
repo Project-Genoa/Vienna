65c2d96ce3761b3949af5db96dd4672761fbe5b0
micheal65536 <>
2024-06-09 11:19:43 +0100

    Fixed bug in Hotbar.limitToInventory()

--- db/src/main/java/micheal65536/vienna/db/model/player/Hotbar.java
+++ db/src/main/java/micheal65536/vienna/db/model/player/Hotbar.java
@@ -3,6 +3,9 @@ package micheal65536.vienna.db.model.player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public final class Hotbar
 {
 	public final Item[] items;
@@ -14,6 +17,8 @@ public final class Hotbar
 
 	public void limitToInventory(@NotNull Inventory inventory)
 	{
+		HashMap<String, Integer> usedStackableItemCounts = new HashMap<>();
+		HashMap<String, HashSet<String>> usedNonStackableItemInstances = new HashMap<>();
 		for (int index = 0; index < this.items.length; index++)
 		{
 			Item item = this.items[index];
@@ -25,7 +30,11 @@ public final class Hotbar
 			{
 				if (inventory.getItemInstance(item.uuid(), item.instanceId()) != null)
 				{
-					continue;
+					HashSet<String> usedItemInstances = usedNonStackableItemInstances.computeIfAbsent(item.uuid(), uuid -> new HashSet<>());
+					if (!usedItemInstances.add(item.instanceId()))
+					{
+						item = null;
+					}
 				}
 				else
 				{
@@ -35,16 +44,15 @@ public final class Hotbar
 			else
 			{
 				int inventoryCount = inventory.getItemCount(item.uuid());
-				if (inventoryCount > 0)
+				int usedCount = usedStackableItemCounts.getOrDefault(item.uuid(), 0);
+				if (inventoryCount - usedCount > 0)
 				{
-					if (inventoryCount < item.count())
+					if (inventoryCount - usedCount < item.count())
 					{
-						item = new Item(item.uuid(), inventoryCount, null);
+						item = new Item(item.uuid(), inventoryCount - usedCount, null);
 					}
-					else
-					{
-						continue;
-					}
+					usedCount += item.count();
+					usedStackableItemCounts.put(item.uuid(), usedCount);
 				}
 				else
 				{
